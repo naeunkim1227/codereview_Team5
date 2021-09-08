480104347d8f040a04e6b4e8cdd6c6f25e1933bd
naeunkim1227 <kne0140@daum.net>
2021-09-08 18:53:01 +0900

    21.09.08 first commit

---
+++ helloworld/src/com/douzone/helloworld/helloworld.java
@@ -0,0 +1,18 @@
+package com.douzone.helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		/*helloworld[Eclipse Native Project]
+			|---src
+			|---bin
+			|---.project
+			|---.classpath
+			|---/.settings
+		*/
+
+	}
+
+}
---
+++ helloworld2/src/main/java/com/douzone/helloworld.java
@@ -0,0 +1,10 @@
+package com.douzone;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
