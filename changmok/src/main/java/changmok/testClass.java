cf716f280dc7868f78bd91d12f3e77e1b13b4f9e
changmok <hong1071@nate.com>
2021-09-09 21:59:01 +0900

    210909 창목 commit

--- changmok/src/main/java/changmok/testClass.java
+++ changmok/src/main/java/changmok/testClass.java
@@ -4,6 +4,10 @@ public class testClass {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 
 	}
 
